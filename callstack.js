02aa60cf7962ec410cc5c7508e545ac27f4ae949
sumogashoa <sumogashoa@shoprite.co.za>
2022-01-22 15:18:21 +0200

    call stack commit

---
+++ callstack.js
@@ -0,0 +1,28 @@
+function ceo()
+{
+    console.log("Lets make a dent in the universe")
+    cto()
+
+}
+function cto()
+{
+    console.log("Lets make a gamechanger product")
+    vpengg()
+
+}
+function vpengg(){
+    console.log("Lets make a new Javascript framework")
+    techlead()
+}
+function techlead()
+{
+    console.log("Lets pit Angular to Typescript")
+    developer()
+}
+function developer()
+{
+    console.log("Lets copy some code from stack OF")
+    //developer()
+}
+
+ceo();
